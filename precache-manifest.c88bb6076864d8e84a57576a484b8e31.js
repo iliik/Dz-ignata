7cabbe7efeacabee74fd371cd78b2d83e134b0d7
Ilya <koselevicila@gmail.com>
2022-09-21 11:42:28 +0300

    Updates

---
+++ precache-manifest.c88bb6076864d8e84a57576a484b8e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021ea463152e232e3692b0c096c3c67b",
+    "url": "/Dz-ignata/index.html"
+  },
+  {
+    "revision": "808ea6a7207da85d97bc",
+    "url": "/Dz-ignata/static/css/main.7165006f.chunk.css"
+  },
+  {
+    "revision": "c236e78d3c1dd16f5448",
+    "url": "/Dz-ignata/static/js/2.3196abc5.chunk.js"
+  },
+  {
+    "revision": "40603d66a12b4e76df8998e78935ec3a",
+    "url": "/Dz-ignata/static/js/2.3196abc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808ea6a7207da85d97bc",
+    "url": "/Dz-ignata/static/js/main.c29fc3ea.chunk.js"
+  },
+  {
+    "revision": "58818076cbc34c09f6f2",
+    "url": "/Dz-ignata/static/js/runtime-main.6754873b.js"
+  }
+]);
